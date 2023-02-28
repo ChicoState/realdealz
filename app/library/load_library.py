832d0ad920c9a4e578edd5b4ba540300f46a80d2
Noah Iles <noahiles@gmail.com>
2023-02-27 19:55:10 -0800

    fixed linting

--- app/library/load_library.py
+++ app/library/load_library.py
@@ -1,16 +1,14 @@
-from environs import Env
-from datetime import datetime as dt
-import urllib3
-import json
 import logging as log
 from functools import wraps
+from datetime import datetime as dt
+import json
+import urllib3
+from environs import Env
 
 log.basicConfig(level=log.INFO)
 
-"""_summary_
-    Decorator to check if auth token is expired
-"""
 def val_auth(func):
+    '''For Validating Auth'''
     @wraps(func)
     def inner(self, *args, **kwargs):
         if dt.now().timestamp() - self.auth["request_time"] > self.auth["expires_in"]:
@@ -20,50 +18,48 @@ def val_auth(func):
 
 
 
-# 
-"""
-    # auth[id] - Client ID
-    # secret - Client Secret
-    # auth['Authorization'] - Auth token
-"""
-class Library: 
+class Library:
+    '''
+    auth[id] - Client ID
+    secret - Client Secret
+    auth['Authorization'] - Auth token
+    '''
     def __init__(self, load = True):
         log.info("initializing Library")
-        if not load: return 
+        if not load:
+            return
         env = Env()
         env.read_env()
         self.secret = env.str("Client_secret")
-        self.id = env.str("Client_id")
-        if not self.secret or not self.id:
-            log.error("Client_secret or Client_id is not defined") 
+        self._id = env.str("Client_id")
+        if not self.secret or not self._id:
+            log.error("Client_secret or Client_id is not defined")
         self.auth = self.get_auth_token()
-        self.a_header = {"Client-ID": self.id}
+        self.a_header = {"Client-ID": self._id}
         self.a_header['Authorization'] = self.auth.pop('Authorization', None)
         return
 
-    """
-        Twitch API calls Authentication
-    """
     def get_auth_token(self):
-        # HTTP POST request to get auth token
+        '''HTTP POST request to get auth token'''
         log.info("Getting auth token")
         http = urllib3.PoolManager()
-        authURL = "https://id.twitch.tv/oauth2/token"
-        r = json.loads(http.request(
+        auth_url = "https://id.twitch.tv/oauth2/token"
+        result = json.loads(http.request(
             'POST',
-            authURL,
-            fields={"client_id": self.id, "client_secret": self.secret, "grant_type": "client_credentials"}
+            auth_url,
+            fields={"client_id": self._id, "client_secret": self.secret,
+                     "grant_type": "client_credentials"}
         ).data)
         # clean the data
-        r["request_time"] = dt.now().timestamp()
-        r.pop('token_type', None)
-        r['Authorization'] = 'Bearer' + " " + r.pop('access_token', None)
-        if r['Authorization'] == " ":
+        result["request_time"] = dt.now().timestamp()
+        result.pop('token_type', None)
+        result['Authorization'] = 'Bearer' + " " + result.pop('access_token', None)
+        if result['Authorization'] == " ":
             log.error('Could not get auth token')
-        return r
+        return result
 
-    # Twitch API calls Game Slice
     def search_all(self, content="mario asdf"):
+        '''Twitch API calls Game Slice'''
         _url = "https://api.igdb.com/v4/search"
         _header = {
         }
@@ -73,22 +69,24 @@ class Library:
             "limit": 50,
         }
         return self.m_post(_url, _fields, _header)
-        
+
     @val_auth
-    def m_post(self, url, p_fields={}, p_headers={}):
+    def m_post(self, url : str, p_fields : dict , p_headers : dict):
+        '''Generic method for posting with twitch authentication'''
         http = urllib3.PoolManager()
         _headers = {
             "Content-Type": "application/json"
         }
         _headers.update(self.a_header)
-        _headers.update(p_headers)
-        r = json.loads(http.request(
+        if p_headers is not None:
+            _headers.update(p_headers)
+        result = json.loads(http.request(
             'POST',
             url,
             headers=_headers,
             fields=p_fields
         ).data)
-        return r
+        return result
 
 if __name__ == "__main__":
     library = Library()
