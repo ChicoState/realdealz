a126d75cd325de9256e162bd1a561a22e0f6fa92
Noah Iles <noahiles@gmail.com>
2023-04-07 22:46:15 -0700

    post create, clear models

---
+++ app/clear_models.sh
@@ -0,0 +1,15 @@
+#!/bin/ash
+
+if ps -ef | awk '$1 == 1 && /python3 manage.py runserver/ {exit 0} END {exit 1}'; then
+    echo >&2 "Docker Container or Server Not Detected, Exiting..."
+    exit 1
+fi
+
+LOCAL_DIR="$(pwd)"
+cd /app
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc" -delete
+find . -path "*.sqlite3" -delete
+python manage.py makemigrations
+python manage.py migrate
+cd $LOCAL_DIR
