13977f4b6d65d10f56b9c0949fe5a185223fcf67
NoahIles <31051719+NoahIles@users.noreply.github.com>
2023-04-09 12:32:54 -0700

    Account integration dependencies and urls, Post create model handling (#55)
    * deleted extra html
    
    * added allauth
    
    * removed relative import for portability
    
    * post create, clear models
    
    * fixed some things

--- app/realDealz/models.py
+++ app/realDealz/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 import logging as log
-from . import library as lib
+import realDealz.library as lib
 
 # Model attributes are declared here
 
@@ -13,11 +13,9 @@ class Platform(models.Model):
     def __str__(self):
         '''String for representing the Model object.'''
         return str(self.P)
-    
-#Default value for platforms
-def DefaultPlatforms():
-    return ['PC']
-    
+
+
+
 class Genre(models.Model):
     '''Model representing a game genre'''
     G = models.CharField(max_length=200, default='Action')
@@ -26,9 +24,9 @@ class Genre(models.Model):
         '''String for representing the Model object.'''
         return str(self.G)
     
-def DefaultGenres():
-    return ['Action']
     
+
+
 class Seller(models.Model):
     '''Model representing the source seller'''
     sources = models.URLField(max_length = 200, default='https://www.google.com/')
@@ -36,36 +34,20 @@ class Seller(models.Model):
     def __str__(self):
         '''String for representing the Model object.'''
         return str(self.sources)
-    
-def DefaultSellers():
-    return ['https://www.google.com/']
+        
 
 
-
-
-# Here is an example record from a steamspy GET request 
-# "appid": 570,
-#     "name": "Dota 2",
-#     "developer": "Valve",
-#     "publisher": "Valve",
-#     "score_rank": "",
-#     "positive": 1603433,
-#     "negative": 335731,
-#     "userscore": 0,
-#     "owners": "200,000,000 .. 500,000,000",
-#     "average_forever": 37644,
-#     "average_2weeks": 1338,
-#     "median_forever": 798,
-#     "median_2weeks": 809,
-#     "price": "0",
-#     "initialprice": "0",
-#     "discount": "0",
-#     "ccu": 532383
 
 class Game(models.Model):
     '''Generic Game model'''
-    appid = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, default="Empty Game", help_text='Give title')
+
+    #View the Game object in the admin panel
+    def __str__(self):
+        return f"{self.name} : ${self.price} - made by {self.developer}"
+
+
+    appid = models.IntegerField(primary_key=True, help_text='Unique ID for this particular game')
+    name = models.CharField(max_length=100, default="-1", help_text='Game title')
     price = models.FloatField(default="-1")
     discount = models.CharField(max_length=20, default="-1")
     developer = models.CharField(max_length=100, default="Unknown")
@@ -76,14 +58,11 @@ class Game(models.Model):
     average_2weeks = models.CharField(max_length=100, default="-1")
     cover = models.ImageField(upload_to='images/', default='images/default.png')
 
-
     #Field can include multiple MultiFields. This allows multiple platforms and genres per game entry
-    platform = models.ManyToManyField(Platform, help_text='Select Game platforms', default=DefaultPlatforms)
-    genre = models.ManyToManyField(Genre, help_text='Select Game genres', default=DefaultGenres)
-    vendors = models.ManyToManyField(Seller, help_text='Links to all vendors', default=DefaultSellers)
+    platform = models.ManyToManyField(Platform, help_text='Select Game platforms', default="PC")
+    genre = models.ManyToManyField(Genre, help_text='Select Game genres', default="-1")
 
-    def __str__(self):
-        return f"{self.name} : ${self.price} - made by {self.developer}"
+
 
     @classmethod
     def initial_load(self):
@@ -91,17 +70,24 @@ class Game(models.Model):
         l = lib.Library()
         _games = l.get_top_100()
         for game in _games.keys():
+            _g = _games[game]
+            # If the game is already in the database, skip it
+            if Game.objects.filter(appid=_g['appid']).exists():
+                continue
+            if _g['price'] == 999:  
+                _g['price'] = 0 
+            # We can also verify the price here. 
             _game = Game.objects.create(
-                appid = _games[game]['appid'],
-                name = _games[game]['name'],
-                price = _games[game]['price'],
-                discount = _games[game]['discount'],
-                developer = _games[game]['developer'],
-                publisher = _games[game]['publisher'],
-                positive = _games[game]['positive'],
-                negative = _games[game]['negative'],
-                average_forever = _games[game]['average_forever'],
-                average_2weeks = _games[game]['average_2weeks'],
+                appid = _g['appid'],
+                name = _g['name'],
+                price = _g['price'],
+                discount = _g['discount'],
+                developer = _g['developer'],
+                publisher = _g['publisher'],
+                positive = _g['positive'],
+                negative = _g['negative'],
+                average_forever = _g['average_forever'],
+                average_2weeks = _g['average_2weeks'],
             )
             _game.save()
     
@@ -111,11 +97,12 @@ class Game(models.Model):
         Game.objects.all().delete()
         print("Database cleared")
 
+    #! Deprecated? 
     #When clicking a link, return the respective id of the game
     def get_absolute_url(self):
         '''Returns the URL to access a detail record for this Game.'''
         return reverse('game-detail', args=[str(self.appid)])
 
 if __name__ == "__main__":
-    print("hekkio")
-    Game().initial_load
+    print("Hello World")
+    # Game().initial_load
