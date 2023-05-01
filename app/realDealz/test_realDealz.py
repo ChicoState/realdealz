bd446151d594b115ebfb0a2d1801a9c9c5ad5d2d
root <root@LAPTOP-MMSI4IG9.localdomain>
2023-05-01 14:41:03 -0700

    added smoke test

---
+++ app/realDealz/test_realDealz.py
@@ -0,0 +1,11 @@
+import pytest
+
+##from realDealz.views import game_search
+##from realDealz.models import Game
+
+def test_always_passes():
+    assert True
+
+def test_always_fails():
+    assert False
+    
