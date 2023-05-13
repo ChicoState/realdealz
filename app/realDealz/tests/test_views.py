0e25269dc526ce51956a162c886dfca1ab0e879f
nickhib <nwhibbard@mail.csuchico.edu>
2023-05-13 11:40:59 -0700

    removed user

--- app/realDealz/tests/test_views.py
+++ app/realDealz/tests/test_views.py
@@ -2,15 +2,12 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from realDealz.models import Game
 import json
-from django.contrib.auth.models import User
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         Game.objects.create(
             appid="440"
         )
-        self.user = User.objects.create_user(username='testuser', password='testpass')
-        self.client.login(username='testuser', password='testpass')
     def test_home_GET(self):
         client = Client()
         response = client.get(reverse('home'))
