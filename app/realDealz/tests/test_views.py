389f284ad584b6691449aada569551f7815cf442
nickhib <nwhibbard@mail.csuchico.edu>
2023-05-13 11:20:50 -0700

    added more testing to increase coverage

--- app/realDealz/tests/test_views.py
+++ app/realDealz/tests/test_views.py
@@ -1,14 +1,16 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from realDealz.models import Game
-
-
+import json
+from django.contrib.auth.models import User
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         Game.objects.create(
             appid="440"
         )
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username='testuser', password='testpass')
     def test_home_GET(self):
         client = Client()
         response = client.get(reverse('home'))
@@ -44,11 +46,34 @@ class TestViews(TestCase):
         response = client.get(reverse('game-detail', args=[440]))
         self.assertEquals(response.status_code,200)
         self.assertTemplateUsed(response,'game_detail.html')
+    def test_contact_GET(self):
+        client = Client()
+        response = client.get(reverse('contact'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'contact.html')
+    def test_profile_GET(self):
+        client = Client()
+        response = client.get(reverse('profile'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'profile.html')
+    def test_game_api_GET(self):
+        client = Client()
+        response = client.get(reverse('games_api'))
+        self.assertEquals(response.status_code,200)
+        content = json.loads(response.content)
+        self.assertIn('data', content)
+        self.assertIn('has_next', content)
     
-    #def test_home_POST_Steam(self):
+    
+    ##def test_home_POST_Steam(self):
      #   client = Client()
       #  response = client.post(reverse('home'), {'steam-login': True})
       #  self.assertEquals(response.status_code,302)## 302 = redirect
-      #  self.assertTemplateUsed(response,'home.html')
+      #  if self.user.is_authenticated:
+      #      self.assertRedirects(response, reverse('catalog'), fetch_redirect_response=False)
+      #  else:
+      #      self.assertRedirects(response, '/accounts/steam/login/', fetch_redirect_response=False)
 
+        # Check that the expected template is used
+      #  self.assertTemplateUsed(response, 'home.html')
   
